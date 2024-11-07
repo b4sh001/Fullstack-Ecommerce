53fdbd27b31eef44630491706e65d2ebccb53445
Joshua Sol <joshuasol4950@gmail.com>
2024-11-08 06:04:26 +0800

    added cors config

--- server/server.js
+++ server/server.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 
 
-mongoose.connect("mongodb+srv://joshuasol4950:joshuasol4950@cluster0.3apza.mongodb.net/").then(console.log("MongoDB Connected")).catch((error) => console.log(er))
+mongoose.connect("mongodb+srv://joshuasol4950:joshuasol4950@cluster0.3apza.mongodb.net/").then(() => console.log("MongoDB Connected")).catch((error) => console.log(er))
 
 
-const app = express()
+const app = express();
+const PORT = process.env.PORT || 5000
+
+
+use.app(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: [
+            "Content-Type",
+            'Authorization',
+            'Cache-Controls',
+            'Expires',
+            'Pragma'
+        ],
+        credentials: true
+    })
+)
